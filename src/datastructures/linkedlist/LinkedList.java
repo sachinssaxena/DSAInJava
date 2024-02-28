5ee8314ce32f5d664848d8d7696ca24fe8697e35
Sachin Saxena <sachinsaxena@outlook.in>
2024-02-28 22:07:12 +0530

    Add LinkedList initial logic

---
+++ src/datastructures/linkedlist/LinkedList.java
@@ -0,0 +1,52 @@
+package datastructures.linkedlist;
+
+public class LinkedList {
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    Node head;
+    Node tail;
+    int length;
+
+    LinkedList(int value) {
+        Node newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
---
+++ src/datastructures/linkedlist/Main.java
@@ -0,0 +1,19 @@
+package datastructures.linkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+
+
+    }
+
+}
